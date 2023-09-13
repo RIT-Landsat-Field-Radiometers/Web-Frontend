82fd935aa4a633e0e0860a1114188bc48ad137db
auztinmartinez <martinezaustin11@gmail.com>
2023-09-13 11:33:49 -0400

    Edited radiometers page, specific board pages, and added datetime API.

--- pages/radiometers/[id].tsx
+++ pages/radiometers/[id].tsx
@@ -1,6 +1,9 @@
+import React from "react";
 import Link from "next/link"
 import { useRouter } from "next/router"
-import {Tabs, Tab, Card, CardHeader, CardBody, Button, Divider} from "@nextui-org/react";
+import {Tabs, Tab, Card, CardHeader, CardBody, Button, Divider, Input,
+  Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure,
+  Select, SelectItem, RadioGroup, Radio} from "@nextui-org/react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -12,6 +15,7 @@ import {
   Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
+import { months } from '../../components/datetimeDetails'
 
 ChartJS.register(
   CategoryScale,
@@ -95,7 +99,22 @@ export default function Data(props) {
     }
   ];
 
+  const arrayRange = (start, stop, step) =>
+    Array.from(
+    { length: (stop - start) / step + 1 },
+    (value, index) => start + index * step
+    );
+
+  const days = arrayRange(1, 31, 1);
+  const years = arrayRange(2020, 2023, 1);
+
   const router = useRouter()
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+  // function to know what radio option is selected
+  const [selected, setSelected] = React.useState("single");
+  // functions for holding the start and end date selections
+  const [startDate, setStartDate] = React.useState({ month: '', day: '', year: '' });
+  const [endDate, setEndDate] = React.useState({ month: '', day: '', year: '' });
   return (
     <>
       <p>
@@ -114,9 +133,149 @@ export default function Data(props) {
                 <p className="text-default-500 text-tiny">{props.radiometer.coordinates}</p>
             </div>
         </div>
-        <Button onPress={() => router.push("/radiometers/" + props.radiometer.id)} color="primary" radius="lg" size="lg" className="px-9">
-            Export Data
-        </Button>
+        <Button onPress={onOpen} color="primary">Export</Button>
+        <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center"  radius="lg" size="lg" className="px-9">
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1">Export Data</ModalHeader>
+                <ModalBody>
+                  <p>Start date</p>
+                  <div className="flex flex-rows">
+                    <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+                      <Select 
+                        label="Month" 
+                        placeholder="Select a month" 
+                        className="max-w-xs"
+                        onChange={(event) => {
+                          const selectedMonth = event.target.value;
+                          setStartDate({ ...startDate, month: selectedMonth });
+                        }}
+                      >
+                        {months.map((month) => (
+                          <SelectItem key={month.short} value={month.short}>
+                            {month.name}
+                          </SelectItem>
+                        ))}
+                      </Select>
+                    </div>
+                    <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+                      <Select 
+                        label="Day" 
+                        placeholder="Select a day" 
+                        className="max-w-xs"
+                        onChange={(event) => {
+                          const selectedDay = event.target.value;
+                          setStartDate({ ...startDate, day: selectedDay });
+                        }}
+                      >
+                        {days.map((day) => (
+                          <SelectItem key={day} value={day}>
+                            {day.toString()}
+                          </SelectItem>
+                        ))}
+                      </Select>
+                    </div>
+                    <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+                      <Select 
+                        label="Year" 
+                        placeholder="Select a year" 
+                        className="max-w-xs"
+                        onChange={(event) => {
+                          const selectedYear = event.target.value;
+                          setStartDate({ ...startDate, year: selectedYear });
+                        }}
+                      >
+                        {years.map((year) => (
+                          <SelectItem key={year} value={year}>
+                            {year.toString()}
+                          </SelectItem>
+                        ))}
+                      </Select>
+                    </div>
+                  </div>
+
+                  <p>End date</p>
+                  <div className="flex flex-rows">
+                    <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+                      <Select 
+                        label="Month" 
+                        placeholder="Select a month" 
+                        className="max-w-xs"
+                        onChange={(event) => {
+                          const selectedMonth = event.target.value;
+                          setEndDate({ ...endDate, month: selectedMonth });
+                        }}
+                      >
+                        {months.map((month) => (
+                          <SelectItem key={month.short} value={month.short}>
+                            {month.name}
+                          </SelectItem>
+                        ))}
+                      </Select>
+                    </div>
+                    <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+                      <Select 
+                        label="Day" 
+                        placeholder="Select a day" 
+                        className="max-w-xs"
+                        onChange={(event) => {
+                          const selectedDay = event.target.value;
+                          setEndDate({ ...endDate, day: selectedDay });
+                        }}
+                      >
+                        {days.map((day) => (
+                          <SelectItem key={day} value={day}>
+                            {day.toString()}
+                          </SelectItem>
+                        ))}
+                      </Select>
+                    </div>
+                    <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+                      <Select 
+                        label="Year" 
+                        placeholder="Select a year" 
+                        className="max-w-xs"
+                        onChange={(event) => {
+                          const selectedYear = event.target.value;
+                          setEndDate({ ...endDate, year: selectedYear });
+                        }}
+                      >
+                        {years.map((year) => (
+                          <SelectItem key={year} value={year}>
+                            {year.toString()}
+                          </SelectItem>
+                        ))}
+                      </Select>
+                    </div>
+                  </div>
+
+                  <div className="flex py-2 px-1 justify-between">
+                    <RadioGroup 
+                      label="Export format" 
+                      orientation="vertical" 
+                      defaultValue="single"
+                      value={selected}
+                      onValueChange={setSelected}
+                    >
+                      <Radio value="single">Single combined CSV file</Radio>
+                      <Radio value="multiple">Individual day CSV files (ZIP)</Radio>
+                    </RadioGroup>
+                    <p className="text-default-500 text-small">Selected: {selected}</p> 
+                  </div>
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="danger" variant="flat" onPress={onClose}>
+                    Close
+                  </Button>
+                  <Button color="primary" onPress={() => handleExportData(startDate, endDate, props.radiometer.id, onClose)}>
+                    Export
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
       </CardHeader>
       <Divider/>
       <CardBody className="flex">
@@ -138,7 +297,7 @@ export default function Data(props) {
       <Divider/>
       <CardBody>
         <h1 className="text-large font-bold">Evironmental Sensor Data</h1>
-        <p>{props.serialNum}</p>
+        {/* <p>{props.serialNum}</p> */}
         {/* <Line options={options} data={props.data} />; */}
       </CardBody>
     </Card>
@@ -169,14 +328,44 @@ export async function getStaticProps(context) {
   const info = await response1.json()
   const theRadiometer = info.radiometers.filter(radiometer => radiometer.id === context.params.id)[0]
   // get the radiometer data
-  const response2 = await fetch("http://52.204.169.92:3000/getRadiometerData/" + theRadiometer.id)
-  const data = await response2.json()
+  // const response2 = await fetch("http://52.204.169.92:3000/getRadiometerData/" + theRadiometer.id)
+  // const data = await response2.json()
 
   return {
     props: {
       radiometer: theRadiometer,
       name: theRadiometer.name,
-      serialNum: data.commsSerial
+      // serialNum: data.commsSerial
     }
   }
 }
+
+const handleExportData = async (startDate, endDate, radiometerID, onClose) => {
+  console.log("Start")
+  console.log(startDate.month);
+  console.log(startDate.day);
+  console.log(startDate.year);
+  console.log("End")
+  console.log(endDate.month);
+  console.log(endDate.day);
+  console.log(endDate.year);
+
+
+  // request data with the selected start and end dates
+  // const response = await fetch('http://52.204.169.92:3000/getRadiometerData/?startDate=${startDate.month}&endDate=${endDate.month}');
+  // const data = await response.json();
+
+  // process the retrieved data or trigger the export process
+  // 
+
+  // reset the start and end dates 
+  startDate.month = ''
+  startDate.day = ''
+  startDate.year = ''
+  endDate.month = ''
+  endDate.day = ''
+  endDate.year = ''
+  
+  // close the modal after exporting
+  onClose();
+};
